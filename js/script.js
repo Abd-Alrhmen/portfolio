db12371784ef83bb4099a211eca4ee7e1590185b
Abd-Alrhmen <andoahmed94@gmail.com>
2024-09-17 00:41:47 +0200

    Edit the fonts and add margin bottom portfolio box

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 // ==========================sticky navebar============================//
 let menuIcon = document.querySelector("#menu-icons");
 let navbar = document.querySelector(".navbar");
-
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bxs-x-circle");
   navbar.classList.toggle("active");
 };
-
 // ==========================sticky navebar============================//
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
-
 window.onscroll = () => {
   sections.forEach((sec) => {
     let top = window.scrollY;
@@ -29,14 +26,11 @@ window.onscroll = () => {
   });
   // ==========================laine bottom navebar============================//
   let header = document.querySelector("header");
-
   header.classList.toggle("sticky", window.scrollY > 100);
-
   // ==========================remove navbar in scroll============================//
   menuIcon.classList.remove("bxs-x-circle");
   navbar.classList.remove("active");
 };
-
 // ===============scroll reveal===============
 ScrollReveal({
   reset: true,
@@ -44,7 +38,6 @@ ScrollReveal({
   duration: 2000,
   delay: 200,
 });
-
 ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
 ScrollReveal().reveal(
   ".home-img, .services-container, .portfolio-box, .contact form",
