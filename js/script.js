1cbb54e5754844ee1c8d4596257ff9e20706b539
Abd-Alrahman <andoahmed94@gmail.com>
2025-06-22 04:18:01 +0300

    Projects added and section removed

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bxs-x-circle");
   navbar.classList.toggle("active");
 };
+
 // ==========================sticky navebar============================//
 let sections = document.querySelectorAll("section");
 let navlinks = document.querySelectorAll("header nav a");
@@ -31,47 +32,13 @@ window.onscroll = () => {
   menuIcon.classList.remove("bxs-x-circle");
   navbar.classList.remove("active");
 };
-// ===============scroll reveal===============
-// ScrollReveal({
-//   reset: true,
-//   distance: "80px",
-//   duration: 2000,
-//   delay: 200,
-// });
-// ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
-// ScrollReveal().reveal(
-//   ".home-img, .services-container, .portfolio-box, .contact form",
-//   { origin: "bottom" }
-// );
-// ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
-// ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
+
 // =============================typed js=============
 const typed = new Typed(".multiple-text", {
-  strings: ["FrontEnd Developer", "Ui Ux Designer"],
+  strings: ["FrontEnd Developer", "FrontEnd Developer"],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
 });
 
-
-// Email Send 
-//   (function(){
-//     emailjs.init("RJy9f6tMPRASpW5cX"); });
-// function sendEmail() {
-//   let templateParams = {
-//     from_name: document.getElementById("FullName").value,
-//     from_email: document.getElementById("emailAddress").value,
-//     mobile: document.getElementById("mobileNumber").value,
-//     subject: document.getElementById("EmailSubject").value,
-//     message: document.getElementById("Message").value
-//   };
-
-//   emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", templateParams)
-//     .then(response => {
-//       alert("Message Sent Successfully!");
-//       document.querySelector("form").reset();
-//     }, error => {
-//       alert("Failed to send message. Please try again.");
-//     });
-// }
